b1c9bee847eaebd27b07d20766053d4c992ae7f3
Jessica Sadler <jessicagsadler@gmail.com>
2021-10-31 19:32:25 +0100

    created screenshots for each driver

--- test_scrive.py
+++ test_scrive.py
@@ -2,11 +2,14 @@ import time
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome import service
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common import by
+import webdriver_manager
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from webdriver_manager.manager import DriverManager
 
 # Create a selenium test that does the following:
     # Go to https://staging.scrive.com/t/9221714692410699950/7348c782641060a9
@@ -32,9 +35,14 @@ def test_scrive():
     chrome_service = Service(ChromeDriverManager().install())
     chrome_driver = webdriver.Chrome(service = chrome_service)
 
+#     sauce_labs_driver = webdriver.Remote(
+#     "https://oauth-jessicagsadler-24155:35879740-f381-4874-8a9d-bb984e530d51@ondemand.eu-central-1.saucelabs.com:443/wd/hub"
+# )
+
     drivers = []
     drivers.append(chrome_driver)
     drivers.append(firefox_driver)
+    # drivers.append(sauce_labs_driver)
     for driver in drivers:
         # Go to https://staging.scrive.com/t/9221714692410699950/7348c782641060a9
         driver.get("https://staging.scrive.com/t/9221714692410699950/7348c782641060a9")
@@ -51,14 +59,14 @@ def test_scrive():
         # driver.save_screenshot("screenshot.png")
 
         element = driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[4]")
-        element.screenshot("element_screenshot_chrome.png")
+        screenshot_name = str(driver.name) + "element_screenshot.png"
+        element.screenshot(screenshot_name)
+        # print(dir(driver))
 
         # Sign the document
         driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[4]/div[1]/a[1]/div").click()
         # Verify that there is a text “Document signed” on the screen.
         verification_message = driver.find_element(By.CSS_SELECTOR, "body > div > div > div.main > div:nth-child(2) > div.scroller > div > div.instructions.s-header-doc-signed > h1 > span")
-        print("verification text: ")
-        print(verification_message.text)
         verification_txt = verification_message.text
 
         assert verification_txt == "Document signed!", "Oops."
