08bef2f036d349111d646734b9a780d2988794dc
Jessica Sadler <jessicagsadler@gmail.com>
2021-10-31 19:34:51 +0100

    deleted conftest.py

--- conftest.py
+++
@@ -1,9 +0,0 @@
-# import pytest
-# from selenium import webdriver
-# from webdriver_manager.chrome import ChromeDriverManager
-
-# @pytest.fixture(scope="session")
-# def chrome_driver():
-#    driver = webdriver.Chrome(ChromeDriverManager().install())
-#    yield driver
-#    driver.quit()
