64b46e5c38c1a2a6b8cec858c2fa1394f47d4596
Jessica Sadler <jessicagsadler@gmail.com>
2021-10-31 14:37:53 +0100

    updated test to run with 2 different browser drivers

--- test_scrive.py
+++ test_scrive.py
@@ -1,8 +1,10 @@
 import time
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome import service
 from selenium.webdriver.common import by
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
@@ -22,34 +24,42 @@ from selenium.webdriver.common.by import By
 # if you have more time. But completing all steps will be considered a bonus rather than describing it.
 # Send the results as a compressed file via email.
 
-@pytest.fixture(scope="session")
-def chrome_driver():
-   s=Service(ChromeDriverManager().install())
-   driver = webdriver.Chrome(service=s)
-   yield driver
-   driver.quit()
+def test_scrive():
 
-def test_scrive(chrome_driver):
-    # Go to https://staging.scrive.com/t/9221714692410699950/7348c782641060a9
-    chrome_driver.get("https://staging.scrive.com/t/9221714692410699950/7348c782641060a9")
-    chrome_driver.implicitly_wait(10)
-    chrome_driver.maximize_window()
-    # Fill in the full name in the document.
-    name_box = chrome_driver.find_element(By.ID,"name")
-    name_box.send_keys("Jessica Sadler")
-    # Click on Next
-    chrome_driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[4]/div[1]/a[1]/div[1]").click()
-    # Take a screenshot of confirmation modal
-    time.sleep(2)
-    chrome_driver.save_screenshot("screenshot.png")
-    # Sign the document
-    chrome_driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[4]/div[1]/a[1]/div").click()
-    # Verify that there is a text “Document signed” on the screen.
-    verification_message = chrome_driver.find_element(By.CSS_SELECTOR, "body > div > div > div.main > div:nth-child(2) > div.scroller > div > div.instructions.s-header-doc-signed > h1 > span")
-    print("verification text: ")
-    print(verification_message.text)
-    verification_txt = verification_message.text
+    firefox_service = Service(executable_path=GeckoDriverManager().install())
+    firefox_driver = webdriver.Firefox(service = firefox_service)
 
-    assert verification_txt == "Document signed!", "Oops."
+    chrome_service = Service(ChromeDriverManager().install())
+    chrome_driver = webdriver.Chrome(service = chrome_service)
 
-    
+    drivers = []
+    drivers.append(chrome_driver)
+    drivers.append(firefox_driver)
+    for driver in drivers:
+        # Go to https://staging.scrive.com/t/9221714692410699950/7348c782641060a9
+        driver.get("https://staging.scrive.com/t/9221714692410699950/7348c782641060a9")
+        driver.implicitly_wait(10)
+        driver.maximize_window()
+        # Fill in the full name in the document.
+        name_box = driver.find_element(By.ID,"name")
+        name_box.send_keys("Jessica Sadler")
+        # Click on Next
+        driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[4]/div[1]/a[1]/div[1]").click()
+                
+        # # Take a screenshot of confirmation modal
+        time.sleep(2)
+        # driver.save_screenshot("screenshot.png")
+
+        element = driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[4]")
+        element.screenshot("element_screenshot_chrome.png")
+
+        # Sign the document
+        driver.find_element(By.XPATH, "/html/body/div/div/div[3]/div[4]/div[1]/a[1]/div").click()
+        # Verify that there is a text “Document signed” on the screen.
+        verification_message = driver.find_element(By.CSS_SELECTOR, "body > div > div > div.main > div:nth-child(2) > div.scroller > div > div.instructions.s-header-doc-signed > h1 > span")
+        print("verification text: ")
+        print(verification_message.text)
+        verification_txt = verification_message.text
+
+        assert verification_txt == "Document signed!", "Oops."
+        driver.quit()
